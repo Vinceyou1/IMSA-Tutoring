7d97aeb6dd7d827253e8964fe8bcec53eedaeb5f
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-04 00:19:17 -0500

    Created simple login and landing page

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,9 +1,16 @@
+"use client"
+import initFirebase from '@/firebase/clientApp';
+import { Button } from '@mui/material';
+import { getAuth } from 'firebase/auth';
 import Image from 'next/image'
 
 export default function Hello() {
+  initFirebase();
+  const auth = getAuth();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <p>Hello World!</p>
+      <Button onClick={() => auth.signOut()}>Sign Out</Button>
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import initFirebase from '@/firebase/clientApp';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { Button } from '@mui/material';
 import { NextPage } from 'next';
-import {useAuthState} from 'react-firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/navigation';
+import Loading from './loading';
 
 // Import the functions you need from the SDKs you need
 
@@ -19,11 +20,11 @@ const Home: NextPage = () => {
   const router = useRouter();
 
   if(loading){
-    return (<div>Loading...</div>)
+    return (<Loading />)
   }
   if(user){
     router.push("/hello")
-    return (<div>Loading...</div>)
+    return (<Loading />)
   }
 
   const signIn = async () => {
@@ -32,9 +33,16 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className='h-screen'>
-      <p>Hello!</p>
-      <Button variant='contained' onClick={signIn}>Sign In</Button>
+    <div className='h-full flex items-center justify-center'>
+      <div>
+          <h1 className='text-center'>Welcome to IMSA Tutoring!</h1>
+          <h3 className='text-center'>Sign in with your IMSA email to get started</h3>
+          <div className='h-12'></div>
+          <div className='flex justify-center'>
+            <Button  variant='contained' onClick={signIn}>Sign In</Button>
+          </div>
+      </div>
+      
     </div>
   )
 }
