64f754f8fe2cbd34cbf12264cd2c7bd26ff82571
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-04 00:19:00 -0500

    Created Loading Page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import './loading.css'
+
+export default function Loading(){
+    return (
+        <div className="h-full w-full">
+            <div className="lds-dual-ring align-middle"></div>
+        </div>
+    )
+}
