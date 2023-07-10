50474e4f22297ec3bdd01e269859490dcf3d0190
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-09 23:32:43 -0500

    Reformatted button

--- app/request.tsx
+++ app/request.tsx
@@ -9,7 +9,7 @@ export default function Request({request}:{request:DocumentJSON}){
     let button;
     if(request.data.uid == uid) {
         button = (
-            <button className="border-solid border-[red] border-2 pr-6 pl-6 rounded font-bold text-[red]">
+            <button className="border-solid h-fit p-4 border-[red] border-2 rounded font-bold text-[red]">
                 DELETE</button>
         )
     } else {
@@ -19,14 +19,17 @@ export default function Request({request}:{request:DocumentJSON}){
         )
     }
     return(
-        <div className="flex flex-row justify-between
+        <div className="flex flex-row justify-between items-center
         bg-request dark:bg-request-dark max-w-[100%] break-words m-2 mr-0 p-2 rounded">
             <span>
                 <p>{request.data.name}</p>
                 <p>{request.data.class} with {request.data.teacher}</p>
                 <p>{request.data.info}</p>
             </span>
+            <div>
+
             {button}
+            </div>
         </div>
             
     )
