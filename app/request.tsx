f3f36a231bb02b8d59414a4a32fc958ecdac9036
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-09 19:02:38 -0500

    Decided to remove dynamic request page

--- app/request.tsx
+++ app/request.tsx
@@ -1,13 +1,33 @@
 import { useRouter } from "next/navigation";
-import { RequestJSON } from "./page";
+import { DocumentJSON } from "./page";
+import { useContext } from "react";
+import { FirebaseAuthContext } from "./contexts/FirebaseAuthContext";
 
-export default function Request({request}:{request:RequestJSON}){
+export default function Request({request}:{request:DocumentJSON}){
     const router = useRouter();
+    const uid = useContext(FirebaseAuthContext).currentUser?.uid;
+    let button;
+    if(request.data.uid == uid) {
+        button = (
+            <button className="border-solid border-[red] border-2 pr-6 pl-6 rounded font-bold text-[red]">
+                DELETE</button>
+        )
+    } else {
+        button = (
+            <button className="border-solid border-secondary dark:border-secondary-dark border-2 pr-6 pl-6 rounded font-bold ">
+                CLAIM</button>
+        )
+    }
     return(
-        <a href={"/request/" + request.id}
-            className="bg-request block dark:bg-request-dark max-w-[100%] break-words m-2 mr-0 p-2 rounded cursor-pointer hover:bg-[#193769] dark:hover:text-primary hover:text-primary">
-            <p>{request['data']['name']} - {request['data']['subject']}</p>
-            <p>{request['data']['class']} with {request['data']['teacher']}</p>
-        </a>
+        <div className="flex flex-row justify-between
+        bg-request dark:bg-request-dark max-w-[100%] break-words m-2 mr-0 p-2 rounded">
+            <span>
+                <p>{request.data.name}</p>
+                <p>{request.data.class} with {request.data.teacher}</p>
+                <p>{request.data.info}</p>
+            </span>
+            {button}
+        </div>
+            
     )
 }
