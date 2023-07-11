b3e4e13f289e8738f14e51aa50112c12b93e438a
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-10 22:28:10 -0500

    Added filter ability and combined returns

--- app/home_components/filter.tsx
+++ app/home_components/filter.tsx
@@ -2,26 +2,52 @@
 
 import { useState } from "react";
 import { Filter } from "../page";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+
+function FilterClass(classname: string, filter:Filter, updateFilter: React.Dispatch<React.SetStateAction<Filter>>){
+    function removeClass(){
+        let temp:Filter = {
+            classes: []
+        };
+        filter.classes.forEach(element => {
+            if(element != classname){
+                temp.classes.push(element);
+            }
+        });
+        updateFilter(temp);
+    }
+    return (
+        <div className="p-2 rounded-full bg-primary dark:bg-primary-dark border-2 border-secondary dark:border-secondary-dark">
+            {classname}
+            <button onClick={removeClass}>X</button>
+        </div>
+    )
+}
 
 export default function Filter({filter, updateFilter} : {filter: Filter, updateFilter: React.Dispatch<React.SetStateAction<Filter>>}){
     const [active, setActive] = useState(false);
-    if(!active){
-        return(
-            <div onClick={() => setActive(true)} className={"ease-in-out before:top-[50%] duration-300 h-[50%] w-[100%] fixed top-[92.5%]"}>
-                <h3 className="trailing rounded-t-xl border-t-8 box-border border-t-secondary dark:border-t-secondary-dark text-center text-primary dark:text-primary-dark text-sm pl-4 h-[8vh] bg-secondary dark:bg-secondary-dark">
-                    &#9650; <br/>
-                    Filters
-                </h3>
-            </div>
-        )
-    }
+    const isMobile = screen.height > screen.width;
+    const grid_elements = isMobile ? 3 : 12;
+    let styles = active ? "before:top-[95%] duration-500 bg-primary dark:bg-primary-dark top-[25%]": 
+        "before:top-[50%] duration-300 top-[92.5%]";
+    let arrow = active ? '\u25BC' : '\u25B2';
     return(
-        <div onClick={() => setActive(false)} className={
-            "before:top-[95%] ease-in-out duration-500 h-[75%] bg-primary dark:bg-primary-dark w-[100%] fixed top-[25%]"}>
-            <h3 className="trailing rounded-t-xl border-t-8 box-border border-t-secondary dark:border-t-secondary-dark text-center text-primary dark:text-primary-dark text-sm pl-4 h-[8vh] bg-secondary dark:bg-secondary-dark">
-                &#9660; <br/>
+        <div className={styles + " h-[75%] ease-in-out w-[100%] fixed"}>
+            <h3 onClick={() => setActive(!active)} className="cursor-pointer trailing rounded-t-xl border-t-8 box-border border-t-secondary dark:border-t-secondary-dark text-center text-primary dark:text-primary-dark text-sm pl-4 h-[8vh] bg-secondary dark:bg-secondary-dark">
+                {arrow} <br/>
                 Filters
             </h3>
+            <Grid2 container sx={{
+                margin: 1
+            }}>
+                {filter.classes.map((classname) =>{
+                    return(
+                        <Grid2 xs={12/grid_elements}>
+                            {FilterClass(classname, filter, updateFilter)}
+                        </Grid2>
+                    )
+                })}
+            </Grid2>
         </div>
     )
 
